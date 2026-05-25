e9d2ebdd6169590c4392847f512a02411cd74908
deriskey <idrishjauhari196@gmail.com>
2026-05-25 16:26:11 +0800

    feat: containerize network ping utility with multi-OS support

--- ping_checker.py
+++ ping_checker.py
@@ -1,4 +1,5 @@
 import os
+import platform #detects underlying OS
 
 #create a list of IP addresses to monitor.
 #(We will use Google DNS, Cloudflare DNS, and a fake local IP for testing)
@@ -11,13 +12,19 @@ devices_to_ping = [
 print("==STARTING DOSM NETWORK REACHABILITY SCAN ===")
 print("=============================================")
 
+#Determine corect flag based on OS
+current_os = platform.system().lower()
+if current_os == "windows":
+    ping_flag = "-n 1"
+else:
+    ping_flag = "-c 1" #Linux/Alpine
 #loop throught each target IP address in our tracking list
 for ip in devices_to_ping:
 
     #Build native ping commanf string for Windows systems
     #'-n 1' tells Windows to send exactly 1 ping packet instead of 4 (save time)
     #3'>null' hides messy raw command line output from clutteting your screen
-    ping_command = f"ping -n 1 {ip} > nul"
+    ping_command = f"ping {ping_flag} {ip} > /dev/null 2>&1" if current_os != "windows" else f"ping{ping_flag} {ip} > nul"
 
     #Execute comman on system
     #os.system returns 0 if command succeeds (ping replies!)
